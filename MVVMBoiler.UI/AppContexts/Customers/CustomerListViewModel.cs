a5370d47879ad6eaf73623aa2d553e716ecaee7d
Tuan Nam Truong <Truong@Funkinform.de>
2021-06-04 13:51:44 +0200

    Add CurrentViewModel to MainWindow.

--- MVVMBoiler.UI/AppContexts/Customers/CustomerListViewModel.cs
+++ MVVMBoiler.UI/AppContexts/Customers/CustomerListViewModel.cs
@@ -9,17 +9,18 @@ namespace MVVMBoiler.UI.AppContexts.Customers
     public class CustomerListViewModel
     {
         private ICustomersRepository _customersRepository = new CustomersRepository();
-        
+        private ObservableCollection<Customer> _customers;
+
         public CustomerListViewModel()
         {
             // Guard to check if the code is executed  in the designer 
             if(DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
                 return;
-            //Customers = new ObservableCollection<Customer>(_customersRepository.GetAllAsync().Result);
+            //Customers = new ObservableCollection<Customer>(_customersRepository.GetAllAsync().Result); 
             var customersTask = Task.Run(() => _customersRepository.GetAllAsync().Result);
             Customers = new ObservableCollection<Customer>(customersTask.Result);
         }
 
-        public ObservableCollection<Customer> Customers { get; set; }
+        public ObservableCollection<Customer> Customers { get => _customers; set => _customers = value; }
     }
 }
---
+++ MVVMBoiler.UI/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVVMBoiler.UI.AppContexts.Customers;
+
+namespace MVVMBoiler.UI
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new CustomerListViewModel();
+        }
+
+        public object CurrentViewModel { get; set; }
+    }
+}
