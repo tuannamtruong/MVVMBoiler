5b74888b8ad472893430c5a08aa0e4d02856c674
Tuan Nam Truong <Truong@Funkinform.de>
2021-06-04 16:12:50 +0200

    Add Navigation Bar in MainWindow, that allows switch between views.

--- MVVMBoiler.UI/MainWindowViewModel.cs
+++ MVVMBoiler.UI/MainWindowViewModel.cs
@@ -1,14 +1,47 @@
 ﻿using MVVMBoiler.UI.AppContexts.Customers;
+using MVVMBoiler.UI.AppContexts.OrderPrep;
+using MVVMBoiler.UI.AppContexts.Orders;
+using MVVMBoiler.UI.Bases;
+using System;
 
 namespace MVVMBoiler.UI
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
+        private CustomerListViewModel _customerListViewModel = new CustomerListViewModel();
+        private OrderViewModel _orderViewModel = new OrderViewModel();
+        private OrderPrepViewModel _orderPrepViewModel = new OrderPrepViewModel();
+        private ViewModelBase currentViewModel;
+
+        public RelayCommand<string> NavigateCommand { get; set; }
         public MainWindowViewModel()
         {
-            CurrentViewModel = new CustomerListViewModel();
+            //CurrentViewModel = new CustomerListViewModel();
+            NavigateCommand = new RelayCommand<string>(OnNavigate);
         }
 
-        public object CurrentViewModel { get; set; }
+        private void OnNavigate(string destViewModel)
+        {
+            switch(destViewModel)
+            {
+                case nameof(OrderPrepViewModel):
+                    CurrentViewModel = _orderPrepViewModel;
+                    break;
+                case nameof(CustomerListViewModel):
+                    CurrentViewModel = _customerListViewModel;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => currentViewModel; set
+            {
+                currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
